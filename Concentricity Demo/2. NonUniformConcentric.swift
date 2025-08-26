d7a8230c9f67d0aabc97e0b7e7c9b647829bd495
Stewart Lynch <slynch@createchsol.com>
2025-08-26 15:47:18 -0700

    Initial Commit

---
+++ Concentricity Demo/2. NonUniformConcentric.swift
@@ -0,0 +1,36 @@
+//
+//----------------------------------------------
+// Original project: Concentricity Demo
+// by  Stewart Lynch on 2025-08-25
+//
+// Follow me on Mastodon: https://iosdev.space/@StewartLynch
+// Follow me on Threads: https://www.threads.net/@stewartlynch
+// Follow me on Bluesky: https://bsky.app/profile/stewartlynch.bsky.social
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Email: slynch@createchsol.com
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+//----------------------------------------------
+// Copyright © 2025 CreaTECH Solutions. All rights reserved.
+
+
+import SwiftUI
+
+struct NonUniformConcentric: View {
+    @State private var padding = 0.0
+    var body: some View {
+            ZStack{
+                VStack {
+                  
+                }
+                Slider(value: $padding, in: 0...100)
+                    .padding()
+            }
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    NonUniformConcentric()
+}
